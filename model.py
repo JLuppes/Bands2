597beb5fdceecdafb2ff80f8c94645bcaa9595b5
Joshua Luppes <lupjos@dunwoody.edu>
2026-04-14 10:37:50 -0500

    Update models for m-m relationships bands-members

--- model.py
+++ model.py
@@ -11,22 +11,37 @@ class Bands(db.Model):
     BandName = db.Column(db.String(80), nullable=False)
     FormedYear = db.Column(db.Integer)
     HomeLocation = db.Column(db.String(80))
-    # Relationship: One band has many members + albums
-    members = db.relationship('Members', backref='band', lazy=True)
+    # Removed members relationship in favor of memberships
+    # members = db.relationship('Members', backref='band', lazy=True)
+    memberships = db.relationship('Memberships', backref='band', lazy=True)
     albums = db.relationship('Albums', backref='band', lazy=True)
 
 
 class Members(db.Model):
     MemberID = db.Column(db.Integer, primary_key=True)
-    BandID = db.Column(db.Integer, db.ForeignKey(
-        'bands.BandID'), nullable=False)
+    # Removed BandID
+    # BandID = db.Column(db.Integer, db.ForeignKey(
+    #     'band.BandID'), nullable=False)
     MemberName = db.Column(db.String(80), nullable=False)
     MainPosition = db.Column(db.String(80))
+    # Add relationship to memberships table
+    memberships = db.relationship('Memberships', backref='member', lazy=True)
+
+
+class Memberships(db.Model):
+    MembershipID = db.Column(db.Integer, primary_key=True)
+    BandID = db.Column(db.Integer, db.ForeignKey(
+        'band.bandID', nullable=False))
+    MemberID = db.Column(db.Integer, db.ForeignKey(
+        'member.memberID', nullable=False))
+    Position = db.Column(db.String(80))
+    StartYear = db.Column(db.Integer)
+    EndYear = db.Column(db.Integer)  # NULL if still active
 
 
 class Albums(db.Model):
     AlbumID = db.Column(db.Integer, primary_key=True)
     BandID = db.Column(db.Integer, db.ForeignKey(
-        'bands.BandID'), nullable=False)
+        'band.BandID'), nullable=False)
     AlbumTitle = db.Column(db.String(80), nullable=False)
     ReleaseYear = db.Column(db.Integer)
